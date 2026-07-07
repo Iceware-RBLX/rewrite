be3bad0bc1ee28ce8ced11cddd8836ad92c2fc50
xander <89797002+nxtcapi@users.noreply.github.com>
2026-07-07 20:04:05 +0200

    Load key from external source in init.lua

--- files/experiences/car dealership tycoon/init.lua
+++ files/experiences/car dealership tycoon/init.lua
@@ -1,1 +1,3 @@
+local key = loadstring(game:HttpGet("https://raw.githubusercontent.com/Iceware-RBLX/rewrite/refs/heads/main/files/storage/key.lua"))()
 
+key:Create("7801647781d5c3b6628612795af639ef")
