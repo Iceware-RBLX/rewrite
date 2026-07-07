d13d973d9db8be53053926ccd86ed5b6d370b9bb
xander <89797002+nxtcapi@users.noreply.github.com>
2026-07-07 19:59:34 +0200

    Update init.lua

--- files/experiences/driving empire/init.lua
+++ files/experiences/driving empire/init.lua
@@ -1,1 +1,3 @@
+local key = loadstring(game:HttpGet("https://raw.githubusercontent.com/Iceware-RBLX/rewrite/refs/heads/main/files/storage/key.lua"))()
 
+key:Create("d29bd5160e973ceedfe5e0b268e6b32c")
