d8d8f1981e58ea30542952756fa2248b3bedae88
xander <89797002+nxtcapi@users.noreply.github.com>
2026-07-07 20:03:14 +0200

    Load key from external source in init.lua

--- files/experiences/midnight chasers/init.lua
+++ files/experiences/midnight chasers/init.lua
@@ -1,1 +1,3 @@
+local key = loadstring(game:HttpGet("https://raw.githubusercontent.com/Iceware-RBLX/rewrite/refs/heads/main/files/storage/key.lua"))()
 
+key:Create("bb0ddec488ffaaa951b239c94d320547")
