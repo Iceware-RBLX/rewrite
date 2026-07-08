4dd997132b02af4397497dde90e5c589f4f18dcb
xander <89797002+nxtcapi@users.noreply.github.com>
2026-07-08 21:35:41 +0200

    Implement executor validation in init.lua
    Added executor check to prevent unsupported executors.

--- files/experiences/midnight chasers/init.lua
+++ files/experiences/midnight chasers/init.lua
@@ -1,3 +1,8 @@
+if identifyexecutor and (identifyexecutor():lower():find("solara") or identifyexecutor():lower():find("xeno")) then
+    game.Players.LocalPlayer:Kick("Your executor is not supported, please use a better or executor \n\n https://weao.xyz")
+    return
+end
+
 local key = loadstring(game:HttpGet("https://raw.githubusercontent.com/Iceware-RBLX/rewrite/refs/heads/main/files/storage/key.lua"))()
 
 key:Create("bb0ddec488ffaaa951b239c94d320547")
