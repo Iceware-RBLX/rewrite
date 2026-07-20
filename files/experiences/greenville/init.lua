9f77ec318f86fc8050ad2438259ea699fe64d225
xander <89797002+nxtcapi@users.noreply.github.com>
2026-07-20 19:58:03 +0200

    Create init.lua

---
+++ files/experiences/greenville/init.lua
@@ -0,0 +1,3 @@
+local key = loadstring(game:HttpGet("https://raw.githubusercontent.com/Iceware-RBLX/rewrite/refs/heads/main/files/storage/key.lua"))()
+
+key:Create("16fdf3bff83bcce0883b6fc77f36b4a6")
