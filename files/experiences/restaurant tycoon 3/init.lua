ad4628ae13554fd835fcd374463351fd0b57cbcb
xander <89797002+nxtcapi@users.noreply.github.com>
2026-08-05 23:27:37 +0200

    Update and rename init.Lua to init.lua

---
+++ files/experiences/restaurant tycoon 3/init.lua
@@ -0,0 +1,3 @@
+local key = loadstring(game:HttpGet("https://raw.githubusercontent.com/Iceware-RBLX/rewrite/refs/heads/main/files/storage/key.lua"))()
+
+key:Create("fcdbd229255c54dc1cdeb6da084d2963")
