10dffd5ba33f571b1f822006c8b9583e0ce095a2
xander <89797002+nxtcapi@users.noreply.github.com>
2026-08-13 10:59:31 +0200

    Update init.lua

--- files/experiences/ghost driver/init.lua
+++ files/experiences/ghost driver/init.lua
@@ -1,1 +1,3 @@
-print'cute'
+local key = loadstring(game:HttpGet("https://raw.githubusercontent.com/Iceware-RBLX/rewrite/refs/heads/main/files/storage/key.lua"))()
+
+key:Create("4f4f7bb9a8fa4a1917155c6919be16f8")
