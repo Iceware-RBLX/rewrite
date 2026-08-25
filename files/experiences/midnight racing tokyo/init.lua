e971469cc118ac376dde810f6bb798ccc587eecb
xander <89797002+nxtcapi@users.noreply.github.com>
2026-08-25 21:05:00 +0200

    Add init.lua for midnight racing tokyo experience

---
+++ files/experiences/midnight racing tokyo/init.lua
@@ -0,0 +1,3 @@
+local key = loadstring(game:HttpGet("https://raw.githubusercontent.com/Iceware-RBLX/rewrite/refs/heads/main/files/storage/key.lua"))()
+
+key:Create("366f2753870d3f9fd209e7304e03c997")
