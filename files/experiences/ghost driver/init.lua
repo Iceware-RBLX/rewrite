85882414742397b98511b6f3dc2e6fd91df8dd4d
xander <89797002+nxtcapi@users.noreply.github.com>
2026-09-13 23:50:29 +0200

    Implement executor validation and kick unsupported users
    Added a check to kick players using unsupported executors.

--- files/experiences/ghost driver/init.lua
+++ files/experiences/ghost driver/init.lua
@@ -1,3 +1,8 @@
+if identifyexecutor and (identifyexecutor():lower():find("solara") or identifyexecutor():lower():find("xeno")) then
+    game.Players.LocalPlayer:Kick("Your executor is not supported, please use a better executor \n\n https://weao.xyz")
+    return
+end
+
 local key = loadstring(game:HttpGet("https://raw.githubusercontent.com/Iceware-RBLX/rewrite/refs/heads/main/files/storage/key.lua"))()
 
 key:Create("4f4f7bb9a8fa4a1917155c6919be16f8")
